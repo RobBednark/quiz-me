d683798b26b3c1dcda42a85ad825092f4dabddf6
Rob Bednark <rbednark@gmail.com>
2014-02-06 10:05:03 -0800

    Auto commit from vici

--- emailusername/views.py
+++ emailusername/views.py
@@ -1,11 +1,13 @@
-from django.contrib.auth.views import login as django_login,
-                                      logout_then_login
+from django.contrib.auth.views import (login as django_login,
+                                      logout_then_login)
 
 def login(request):
-    return django_login(request=request, 
-                        template_name="login.html",
-                        extra_context=dict(next='/'),
-                        )
+    import ipdb; ipdb.set_trace()
+    response = django_login(request=request, 
+                            template_name="login.html",
+                            extra_context=dict(next='/'),
+                            )
+    return response
 
 def logout(request):
     # By default, will go to settings.LOGIN_URL
