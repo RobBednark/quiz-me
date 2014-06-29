cd3b24c3bf6c12c1aa3dbf8ce36f62bf17fc2567
Rob Bednark <rbednark@gmail.com>
2014-06-28 22:54:17 -0700

    Auto commit from vici

--- questions/tests.py
+++ questions/tests.py
@@ -5,12 +5,37 @@ when you run "manage.py test".
 Replace this with more appropriate tests for your application.
 """
 
-from django.test import TestCase
+from django.test import LiveServerTestCase
 
+# By default, LiveServerTestCase uses port 8081.
+# If you need a different port, then set this.
+# os.environ['DJANGO_LIVE_TEST_SERVER_ADDRESS'] = 'localhost:8000'
 
-class SimpleTest(TestCase):
-    def test_basic_addition(self):
-        """
-        Tests that 1 + 1 always equals 2.
-        """
-        self.assertEqual(1 + 1, 2)
+# phantomjs archives for Windows, OSX, and Linux can be found at: http://phantomjs.org/download.html
+class BrowserTests(LiveServerTestCase):
+    @classmethod
+    def setUpClass(cls):
+        from splinter import Browser
+        # Note that 'firefox' needs to be in your path.
+        # On Mac, can do this by:
+        #   $ cat << THE_END > /usr/local/bin/firefox
+        #   #!/bin/bash
+        #   open /Applications/Firefox.app
+        #   THE_END
+        #   $ chmod a+x /usr/local/bin/firefox
+        #cls.browser = Browser('firefox')
+
+        # Note that in order to use phantomjs, need to "brew install phantomjs"
+        browser = os.environ.get('ROB_SELENIUM_BROWSER', 'phantomjs')
+        cls.browser = Browser(browser)
+
+        super(BrowserTests, cls).setUpClass()
+
+    @classmethod
+    def tearDownClass(cls):
+        cls.browser.quit()
+        super(BrowserTests, cls).tearDownClass()
+
+    def test_1(self):
+        import pdb; pdb.set_trace()
+        pass
