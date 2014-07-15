83ac8a3c6214166601463beacea28dd8d27546c7
Rob Bednark <rbednark@gmail.com>
2014-07-15 08:14:12 -0700

    Auto commit from vici

--- questions/admin.py
+++ questions/admin.py
@@ -29,6 +29,7 @@ class TagAdmin(admin.ModelAdmin):
 class QuestionAdmin(admin.ModelAdmin):
     inlines = [TagQuestionRelationshipInline]
     list_display = ['datetime_added', 'datetime_updated', 'question', 'answer']
+    # list_filter = ['',]
     formfield_overrides = {
             models.TextField: {'widget' : AdminPagedownWidget },
     }
