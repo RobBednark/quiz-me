1d79656de1600842c3bdabf6d81a8d30b85879c7
Rob Bednark <rbednark@gmail.com>
2020-07-24 06:49:43 -0700

    FIX urls.py: make django-debug-toolbar conditional on settings.ENABLE_DJANGO_DEBUG_TOOLBAR
    This was dependent on settings.DEBUG, which would fail if:
      DEBUG=True
      ENABLE_DJANGO_DEBUG_TOOLBAR=False
      django-debug-toolbar package was not installed (only installed if
        "pipenv install -dev")

--- quizme/urls.py
+++ quizme/urls.py
@@ -31,7 +31,7 @@ urlpatterns = [
     url(r'^admin/', admin.site.urls),
 ]
 
-if settings.DEBUG: 
+if settings.ENABLE_DJANGO_DEBUG_TOOLBAR:
     import debug_toolbar
     urlpatterns = [
         path('__debug__/', include(debug_toolbar.urls)),
