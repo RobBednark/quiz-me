290bab8aa66a9db5213dab3736c2ce451da1f7c8
Rob Bednark <rbednark@gmail.com>
2020-08-08 16:56:11 -0700

    NEW admin: add UserTag objects
    UserTag (User tags) objects can now be viewed in the admin.

--- questions/admin.py
+++ questions/admin.py
@@ -3,7 +3,7 @@ from django.db import models
 
 from pagedown.widgets import AdminPagedownWidget
 
-from .models import Answer, Attempt, Tag, Question, Schedule
+from .models import Answer, Attempt, Tag, Question, Schedule, UserTag
 
 
 class AnswerQuestionRelationshipInline(admin.TabularInline):
@@ -66,8 +66,17 @@ class ScheduleAdmin(admin.ModelAdmin):
         'user',
     ]
 
+class UserTagAdmin(admin.ModelAdmin):
+    list_display = [
+        'id',
+        'enabled',
+        'tag',
+        'user',
+        ]
+
 admin.site.register(Answer, AnswerAdmin)
 admin.site.register(Attempt, AttemptAdmin)
 admin.site.register(Tag, TagAdmin)
 admin.site.register(Question, QuestionAdmin)
 admin.site.register(Schedule, ScheduleAdmin)
+admin.site.register(UserTag, UserTagAdmin)
