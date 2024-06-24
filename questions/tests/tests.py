7bf302a37238c9a26142cd10f921977cb4efefc4
Rob Bednark <rbednark@gmail.com>
2024-06-24 07:08:23 -0700

    rm comment

--- questions/tests/tests.py
+++ questions/tests/tests.py
@@ -1,9 +1,3 @@
-"""
-This file demonstrates writing tests using the unittest module. These will pass
-when you run "manage.py test".
-
-Replace this with more appropriate tests for your application.
-"""
 from datetime import datetime, timedelta
 import os
 
